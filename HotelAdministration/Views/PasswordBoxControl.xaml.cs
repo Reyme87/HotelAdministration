22f266c81e408cbe175a0e856e7beede1d0c0147
Reyme87 <astafev.ivan.0505@mail.ru>
2025-10-29 21:15:18 +0300

    Создание локального PasswordBox

---
+++ HotelAdministration/Views/PasswordBoxControl.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace HotelAdministration.Views
+{
+    /// <summary>
+    /// Логика взаимодействия для PasswordBoxControl.xaml
+    /// </summary>
+    public partial class PasswordBoxControl : UserControl
+    {
+        public string Password 
+        { 
+            get => (string)GetValue(PasswordProperty); 
+            set => SetValue(PasswordProperty, value); 
+        }
+
+        public static readonly DependencyProperty PasswordProperty = DependencyProperty.Register("Password", typeof(string), typeof(PasswordBoxControl), new PropertyMetadata());
+
+        public PasswordBoxControl()
+        {
+            InitializeComponent();
+        }
+
+        private void pwdBox_PasswordChanged(object sender, RoutedEventArgs e)
+        {
+            Password = pwdBox.Password;
+        }
+    }
+}
