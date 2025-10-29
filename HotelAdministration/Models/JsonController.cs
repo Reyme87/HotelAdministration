1d4253ac7aebe9b269dcf50a208d5cd2116b29c7
Reyme87 <astafev.ivan.0505@mail.ru>
2025-10-29 21:15:40 +0300

    Добавление модели Administrator. Создание контроллера Json

---
+++ HotelAdministration/Models/JsonController.cs
@@ -0,0 +1,69 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace PasswordManager.Models
+{
+    internal class JsonController<T>
+    {
+        static public ObservableCollection<T> GetInfo(string fileName)
+        {
+            ObservableCollection<T> collection = new ObservableCollection<T>();
+            using (FileStream fs = new FileStream(fileName, FileMode.OpenOrCreate))
+            {
+                FileInfo fileInfo = new FileInfo(fileName);
+                if (fileInfo.Length != 0)
+                {
+                    try
+                    {
+                        collection = System.Text.Json.JsonSerializer.Deserialize<ObservableCollection<T>>(fs);
+                    }
+                    catch
+                    {
+                        MessageBox.Show("Ошибка чтения данных!");
+                    }
+                }
+            }
+            return collection;
+        }
+
+        static public T GetInfo<T>(string fileName) where T : class
+        {
+            using (FileStream fs = new FileStream(fileName, FileMode.OpenOrCreate))
+            {
+                FileInfo fileInfo = new FileInfo(fileName);
+                if (fileInfo.Length != 0)
+                {
+                    try
+                    {
+                        return System.Text.Json.JsonSerializer.Deserialize<T>(fs);
+                    }
+                    catch
+                    {
+                        MessageBox.Show("Ошибка чтения данных!");
+                    }
+                }
+            }
+
+            return null;
+        }
+
+        static public async void LoadInfoAsync<T>(ObservableCollection<T> collection, string fileName)
+        {
+            string json = JsonConvert.SerializeObject(collection, Formatting.Indented);
+            await File.WriteAllTextAsync(fileName, json);
+        }
+
+        static public async void LoadInfoAsync<T>(T targetObject, string fileName)
+        {
+            string json = JsonConvert.SerializeObject(targetObject, Formatting.Indented);
+            await File.WriteAllTextAsync(fileName, json);
+        }
+    }
+}
