6e5703995f1499b2846f57ec2c208535f730cf73
Reyme87 <astafev.ivan.0505@mail.ru>
2025-11-11 00:12:24 +0300

    Добавление атрибутов сущность

--- HotelAdministration/Models/Client.cs
+++ HotelAdministration/Models/Client.cs
@@ -1,31 +1,44 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace HotelAdministration;
 
 public partial class Client
 {
+    [Column("client_id")]
     public int ClientId { get; set; }
 
+    [Column("first_name")]
     public string FirstName { get; set; } = null!;
 
+    [Column("last_name")]
     public string LastName { get; set; } = null!;
 
+    [Column("middle_name")]
     public string? MiddleName { get; set; }
 
+    [Column("phone_number")]
     public string? PhoneNumber { get; set; }
 
+    [Column("email")]
     public string? Email { get; set; }
 
+    [Column("city")]
     public string? City { get; set; }
 
+    [Column("booked_room_id")]
     public int BookedRoomId { get; set; }
 
+    [Column("payed_amount")]
     public int PayedAmount { get; set; }
 
+    [Column("arrival_date")]
     public DateOnly ArrivalDate { get; set; }
 
+    [Column("check_out_date")]
     public DateOnly CheckOutDate { get; set; }
 
+    [NotMapped]
     public virtual Room BookedRoom { get; set; } = null!;
 }
--- HotelAdministration/Models/Employee.cs
+++ HotelAdministration/Models/Employee.cs
@@ -1,25 +1,35 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace HotelAdministration;
 
 public partial class Employee
 {
+    [Column("employee_id")]
     public int EmployeeId { get; set; }
 
+    [Column("current_floor_id")]
     public int CurrentFloorId { get; set; }
 
+    [Column("first_name")]
     public string FirstName { get; set; } = null!;
 
+    [Column("last_name")]
     public string LastName { get; set; } = null!;
 
+    [Column("middle_name")]
     public string? MiddleName { get; set; }
 
+    [Column("phone_number")]
     public string? PhoneNumber { get; set; }
 
+    [Column("status")]
     public string? Status { get; set; }
 
+    [Column("day_of_cleaning")]
     public string? CleaningDay { get; set; }
 
+    [NotMapped]
     public virtual Floor CurrentFloor { get; set; } = null!;
 }
