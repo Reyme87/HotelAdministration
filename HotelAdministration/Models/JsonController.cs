a93246144080a53aa0572ed41dd1618641508e33
Reyme87 <astafev.ivan.0505@mail.ru>
2025-11-17 23:58:03 +0300

    Добавление параметра static

--- HotelAdministration/Models/JsonController.cs
+++ HotelAdministration/Models/JsonController.cs
@@ -1,16 +1,11 @@
 ﻿using Newtonsoft.Json;
-using System;
-using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 
 namespace PasswordManager.Models
 {
-    internal class JsonController<T>
+    internal static class JsonController<T>
     {
         static public ObservableCollection<T> GetInfo(string fileName)
         {
